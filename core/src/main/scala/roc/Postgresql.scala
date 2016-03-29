39768d70328b65d84ae8ba409925bdffd9526f14
Jeffrey N. Davis <Jeffrey.N.Davis@gmail.com>
2016-03-29 10:46:17 -0500

    Cleaned up, documented Startup.
    Startup contained vestiges of initial development when we were simply
    trying to get Roc off the ground. Now that the ClientDispatcher and
    Postgresql Client have been somewhat solidified, the Startup type has
    been cleaned up with appropriate documenation. We rely on finagle
    Stack Params to inject parameters and defaults.
    
    In addition, the Postgresql Client and README received some minor
    tweaks.
    
    This change addresses https://github.com/penland365/roc/issues/24

--- core/src/main/scala/roc/Postgresql.scala
+++ core/src/main/scala/roc/Postgresql.scala
@@ -1,29 +1,52 @@
 package roc
 
-import com.twitter.finagle._
-import com.twitter.finagle.client.{DefaultPool, StackClient, Transporter, StdStackClient}
-import com.twitter.finagle.netty3.{Netty3Transporter, Netty3Listener}
-import com.twitter.finagle.server.{StackServer, Listener, StdStackServer}
-import com.twitter.finagle.service.{RetryPolicy, TimeoutFilter}
+import com.twitter.finagle.client.{DefaultPool, StackClient, StdStackClient, Transporter}
+import com.twitter.finagle.netty3.Netty3Transporter
 import com.twitter.finagle.transport.Transport
-import com.twitter.finagle.util.DefaultTimer
-import com.twitter.util.{Await, Duration, Future}
-import java.net.SocketAddress
+import com.twitter.finagle.{Name, Service, ServiceFactory, Stack}
+import com.twitter.util.{Duration, Future}
 import roc.postgresql.transport.{Packet, PostgresqlClientPipelineFactory}
 import roc.postgresql.{Request, Result, Startup}
 
-trait PostgresqlRichClient { self: com.twitter.finagle.Client[Request, Result] => 
+/** Rich builder methods for [[roc.Postgresql]]
+  *
+  * Supplements a Finagle Client with convenient builder methods for
+  * constructing a Postgresql Client.
+  */
+trait PostgresqlRichClient { self: com.twitter.finagle.Client[Request, Result] =>
 
-  def newRichClient(dest: Name, label: String): postgresql.Client = 
+  /** 
+    * Creates a new `RichClient` connected to a logical destination described
+    * by `dest` with the assigned `label`. The `label` is used to scope Client stats.
+    */
+  def newRichClient(dest: Name, label: String): postgresql.Client =
     postgresql.Client(newClient(dest, label))
 
-  def newRichClient(dest: String): postgresql.Client = 
+  /**
+    * Creates a new `RichClient` connected to the logical destination described
+    * by `dest`.
+    */
+  def newRichClient(dest: String): postgresql.Client =
     postgresql.Client(newClient(dest))
 }
 
+/** A Finagle Client that connects to Postgresql Server(s).
+  *
+  * @example {{{
+  * val client = Postgresql.client
+  *   .withDatabase("postgres")
+  *   .withUserAndPasswd("username", "password")
+  *   .newRichClient("localhost:5432")
+  * }}}
+  */
 object Postgresql extends com.twitter.finagle.Client[Request, Result] 
   with PostgresqlRichClient {
 
+  /**
+    * Implements a Postgresql client in terms of a Finagle Stack Client.
+    * The client inherits a wealth of features from Finagle including connection 
+    * pooling and load balancing.
+    */
   case class Client(
     stack: Stack[ServiceFactory[Request, Result]] = StackClient.newStack,
     params: Stack.Params = StackClient.defaultParams + DefaultPool.Param(
@@ -44,13 +67,22 @@ object Postgresql extends com.twitter.finagle.Client[Request, Result]
     Service[Request, Result] = postgresql.ClientDispatcher(transport, Startup(params))
   override def configured[P](psp: (P, Stack.Param[P])): Client = super.configured(psp)
 
-  def withCredentials(username: String, database: Option[String]): Client = 
-    configured(Startup.Credentials(username, database))
+  /** The database name for this Postgresql Client to connect to.
+    * @param database the name of the database to connect to
+    */
+  def withDatabase(database: String): Client =
+    configured(Startup.Database(database))
 
-  def withPassword(password: String): Client = 
-    configured(Startup.Password(password))
+  /** The username and password for this Postgresql Client to connect with.
+    * @param username the username for the Postgresql conneciton
+    * @param passwd the password for the Postgresql conneciton
+    */
+  def withUserAndPasswd(username: String, passwd: String): Client = 
+    configured(Startup.Credentials(username, passwd))
   }
 
+  /** Used to construct a Client via the builder pattern.
+    */
   val client = Client()
 
   def newClient(dest: Name, label: String): ServiceFactory[Request, Result] = 
--- core/src/main/scala/roc/postgresql/Startup.scala
+++ core/src/main/scala/roc/postgresql/Startup.scala
@@ -3,30 +3,43 @@ package postgresql
 
 import com.twitter.finagle.Stack
 
-object Startup {
+/** Represents information required for establishing a connection with a Postgresql server
+  */
+private[roc] object Startup {
 
-  case class Credentials private(username: String, database: String)
-  object Credentials {
-    def apply(username: String, someDb: Option[String]): Credentials = someDb match {
-      case Some(db) => new Credentials(username, db)
-      case None     => new Credentials(username, username)
-    }
+  /** Represents the database name to connect to
+    * @constructor create a new Database with a database name
+    * @param database the name of a Postgresql Database to connect to
+    */ 
+  case class Database(db: String)
+  implicit object DatabaseParam extends Stack.Param[Database] {
+    val default = new Database("postgres")
   }
 
+  /** Represents the Username / Password combination for a Postgresql Connection
+    * @constructor create a new Credentials with a username and password
+    * @param username the username for a Postgresql connection
+    * @param passwd the password for a Postgresql connection
+    */
+  case class Credentials(username: String, passwd: String)
   implicit object CredentialsParam extends Stack.Param[Credentials] {
-    val default = Credentials("postgres", None)
+    val default = Credentials("postgres", "postgres")
   }
 
-  case class Password(password: String)
-  implicit object PasswordParam extends Stack.Param[Password] {
-    val default = new Password("")
-  }
-
+  /** Builds a valid [[roc.postgresql.Startup]] from a [[com.twitter.finagle.Stack]]
+    * @param params [[com.twitter.finagle.Stack.Params]]
+    */
   def apply(params: Stack.Params): Startup = {
-   val Credentials(u, d) = params[Credentials]
-   val Password(p)       = params[Password]
-   new Startup(u, p, d) 
+    val Database(db) = params[Database]
+    val Credentials(user, passwd) = params[Credentials]
+    new Startup(user, passwd, db)
   }
 }
 
-case class Startup private(username: String, password: String, database: String)
+/** Contains all information for starting a [[roc.postgresql.ClientDispatcher]]
+  * @param username the username for a Postgresql conneciton
+  * @param password the password for a Postgresql connection
+  * @param database the database for a Postgresql connection
+  */
+private[roc] case class Startup private[postgresql](username: String, password: String,
+  database: String)
